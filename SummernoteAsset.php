6dc604dc3943360ef46f70b268d963efc1bccb01
germanigortcev <german.igortcev@gmail.com>
2015-12-14 09:25:25 +0600

    Update SummernoteAsset.php

--- SummernoteAsset.php
+++ SummernoteAsset.php
@@ -8,7 +8,7 @@ use yii\web\AssetBundle;
 class SummernoteAsset extends AssetBundle
 {
     /** @var string */
-    public $sourcePath = '@bower/summernote/dist';
+    public $sourcePath = '@bower/summernote-0.6.16/dist';
     /** @var array */
     public $depends = [
         'yii\bootstrap\BootstrapPluginAsset',
